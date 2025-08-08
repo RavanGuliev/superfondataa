ad36e03d3476bea73f183aacea2f851d758c9688
Ravan <ghullievrevan@gmail.com>
2025-08-08 15:57:25 +0400

    fix

--- server.js
+++ server.js
@@ -4,18 +4,8 @@ const router = jsonServer.router('db/db.json');
 const middlewares = jsonServer.defaults();
 const cors = require('cors');
 
-// CORS-u aktiv et
 server.use(cors());
-
-// Default middleware-ləri əlavə et
 server.use(middlewares);
-
-// JSON Server router-i əlavə et
 server.use(router);
 
-// Dinləmə (local üçündür, Vercel üçün lazım deyil amma saxlamaq olar)
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
-
-module.exports = server; // Vercel üçün export
+module.exports = server;
